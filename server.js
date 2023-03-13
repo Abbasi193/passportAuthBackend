48a6c7c951ded79ba8148d6e7d5f65279381f995
Abbasi193 <shahidabbasi3625@gmail.com>
2023-03-13 19:25:20 +0500

    a

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/auth/google',
 app.get('/auth/google/callback',
     passport.authenticate('google', {
         failureRedirect: '/login', failureMessage: true,
-        successRedirect: '/good'
+        successRedirect: 'https://flutter-124f5.web.app/doc.html'
     })
 );
 
